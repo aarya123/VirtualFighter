e62c4362ebbd09ba4e7f546afa3f955929ad046a
Anubhaw Arya <anubhaw.arya@gmail.com>
2014-10-18 05:49:37 -0400

    Subscribe to accelerometer

--- src/VirtualFighter.c
+++ src/VirtualFighter.c
@@ -1,27 +1,40 @@
 #include "pebble.h"
 
-#define ACCEL_STEP_MS 100
-int i=0;
+#define ACCEL_STEP_MS 50
+#define HISTORY 10
+
 static Window *window;
 
 static AppTimer *timer;
 
 TextLayer *text_layer;
 char text_layer_text[100];
-static void timer_callback(void *data) {
-    AccelData accel = (AccelData) { .x = 0, .y = 0, .z = 0 };
-    accel_service_peek(&accel);    
-    snprintf(text_layer_text,100, "x=%d y=%d z=%d\n", accel.x,accel.y,accel.z);
-    APP_LOG(APP_LOG_LEVEL_INFO, "%s\n", text_layer_text);
+
+static void averageAccel(AccelRawData *data, int num_samples, int *avg){
+    if(num_samples==0)
+        return;
+    for(int i=0;i<num_samples;i++){
+        avg[0]+=data[i].x;
+        avg[1]+=data[i].y;
+        avg[2]+=data[i].z;
+    }
+    avg[0]/=num_samples;
+    avg[1]/=num_samples;
+    avg[2]/=num_samples;
+}
+
+static void accelSubscriber(AccelRawData *data, uint32_t num_samples, uint64_t timestamp){
+    int avg[]={0,0,0};
+    averageAccel(data,num_samples,avg);
+    snprintf(text_layer_text, 100, "x=%d y=%d z=%d\n", avg[0],avg[1],avg[2]);
     text_layer_set_text(text_layer, text_layer_text);
-    timer = app_timer_register(ACCEL_STEP_MS, timer_callback, NULL);
 }
 
 static void window_load(Window *window) {
     text_layer = text_layer_create(GRect(0, 0, 144, 168));
     text_layer_set_background_color(text_layer, GColorClear);
     text_layer_set_text_color(text_layer, GColorBlack);
-    text_layer_set_text(text_layer,"this is a test");
+    text_layer_set_text(text_layer,"Starting up...");
     layer_add_child(window_get_root_layer(window), (Layer*)text_layer);
 }
 
@@ -36,10 +49,7 @@ static void init(void) {
       .unload = window_unload
     });
     window_stack_push(window, true);
-
-    accel_data_service_subscribe(0, NULL);
-
-    timer = app_timer_register(ACCEL_STEP_MS, timer_callback, NULL);
+    accel_raw_data_service_subscribe(HISTORY, accelSubscriber);
 }
 
 static void deinit(void) {
