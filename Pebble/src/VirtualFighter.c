53a969552aa73c538e1033da9254e989a589a105
Anubhaw Arya <anubhaw.arya@gmail.com>
2014-10-18 11:05:00 -0400

    HealthBar

--- Pebble/src/VirtualFighter.c
+++ Pebble/src/VirtualFighter.c
@@ -4,27 +4,35 @@
 
 #include "pebble.h"
 
-#define HISTORY 50
+#define HISTORY 20
 
 static Window *window;
+
+TextLayer *healthBar;
 
 TextLayer *accelText;
 char accelTextArr[50];
 
-TextLayer *maxText;
 char maxTextArr[50];
 int maxX=0, maxY=0, maxZ=0;
 
-TextLayer *minText;
 char minTextArr[50];
 int minX=0, minY=0, minZ=0;
 
+TextLayer *healthTextBottom;
 TextLayer *healthText;
 char healthTextArr[50];
 
 int averageHistory[][3]={{0,0,0},{0,0,0},{0,0,0}};
 int counter=0;
 
+int prevHealth=100;
+static const uint32_t const segments[] = {1000, 100, 1000, 100, 1000};
+VibePattern pat = {
+    .durations = segments,
+    .num_segments = ARRAY_LENGTH(segments)
+};
+int maxWidth,maxHeight;
 /*static int abs(int x){
     if(x<0)
         return -x;
@@ -121,30 +129,56 @@ static void averageAccel(AccelRawData *data, int num_samples, int *avg){
         counter=0;
     }
 }
+float my_sqrt(int num)
+{
+    float a, p, e = 0.001, b;
+    a = num;
+    p = a * a;
+    int nb = 0;
+    while ((p - num >= e) && (nb++ < 40)) {
+        b = (a + (num / a)) / 2;
+        a = b;
+        p = a * a;
+    }
+    return a;
+}
 
 static void accelSubscriber(AccelRawData *data, uint32_t num_samples, uint64_t timestamp){
     int avg[]={0,0,0};
     averageAccel(data,num_samples,avg);
     snprintf(accelTextArr, 50, "x=%d y=%d z=%d", avg[0],avg[1],avg[2]);
+    int mag=(int)my_sqrt(avg[0]*avg[0]+avg[1]*avg[1]+avg[2]*avg[2]);
+    int xy=(int)my_sqrt(avg[0]*avg[0]+avg[1]*avg[1]);
+    int xz=(int)my_sqrt(avg[0]*avg[0]+avg[2]*avg[2]);
+    int yz=(int)my_sqrt(avg[1]*avg[1]+avg[2]*avg[2]);
     snprintf(maxTextArr,50,"Max: x=%d y=%d z=%d",maxX,maxY,maxZ);
     snprintf(minTextArr,50,"Min: x=%d y=%d z=%d",minX,minY,minZ);
     // APP_LOG(APP_LOG_LEVEL_INFO,"%d",(int)num_samples);
     // APP_LOG(APP_LOG_LEVEL_INFO,"%s",accelTextArr);
+    APP_LOG(APP_LOG_LEVEL_INFO,"Full=%d xy=%d xz=%d yz=%d",mag,xy,xz,yz);
     // APP_LOG(APP_LOG_LEVEL_INFO,"%s",maxTextArr);
     // APP_LOG(APP_LOG_LEVEL_INFO,"%s",minTextArr);
     text_layer_set_text(accelText, accelTextArr);
-    text_layer_set_text(maxText, maxTextArr);
-    text_layer_set_text(minText, minTextArr);
 }
 
 void process_tuple(Tuple *t)
 {
     //int key = t->key;
-    APP_LOG(APP_LOG_LEVEL_INFO,"Got something!");
     int value = t->value->int32;
-    APP_LOG(APP_LOG_LEVEL_INFO,"health=%d",value);
+    if(value<=0){
+        vibes_enqueue_custom_pattern(pat);
+    }else if(prevHealth-value==1){
+        vibes_short_pulse();
+    }else if(prevHealth-value==5){
+        vibes_double_pulse();
+    }
     snprintf(healthTextArr, 50, "Health=%d", value);
     text_layer_set_text(healthText, healthTextArr);
+    text_layer_set_text(healthTextBottom, healthTextArr);
+    int prop=maxHeight*value/100;
+    layer_set_frame((Layer*)healthBar,GRect(0,maxHeight-prop,144,prop));
+    text_layer_set_background_color(healthBar, GColorBlack);
+    prevHealth=value;
 }
 
 static void in_received_handler(DictionaryIterator *iter, void *context)
@@ -158,33 +192,33 @@ static void in_received_handler(DictionaryIterator *iter, void *context)
 }
 
 static void window_load(Window *window) {
-    accelText = text_layer_create(GRect(0, 0, 144, 25));
-    maxText=text_layer_create(GRect(0,25,144,25));
-    minText=text_layer_create(GRect(0,50,144,25));
-    healthText=text_layer_create(GRect(0,75,144,25));
+    healthText=text_layer_create(GRect(0,0,144,25));
+    accelText = text_layer_create(GRect(0, 25, 144, 25));
+    healthTextBottom=text_layer_create(GRect(0,120,144,25));
+    GRect rect=layer_get_frame(window_get_root_layer(window));
+    maxWidth=rect.size.w;
+    maxHeight=rect.size.h;
+    healthBar = text_layer_create(rect);
+    text_layer_set_background_color(healthBar, GColorBlack);
     text_layer_set_background_color(accelText, GColorClear);
-    text_layer_set_background_color(maxText, GColorClear);
-    text_layer_set_background_color(minText, GColorClear);
     text_layer_set_background_color(healthText, GColorClear);
+    text_layer_set_background_color(healthTextBottom, GColorClear);
     text_layer_set_text_color(accelText, GColorBlack);
-    text_layer_set_text_color(maxText, GColorBlack);
-    text_layer_set_text_color(minText, GColorBlack);
     text_layer_set_text_color(healthText, GColorBlack);
+    text_layer_set_text_color(healthTextBottom, GColorWhite);
     text_layer_set_text(accelText,"Starting up...");
-    text_layer_set_text(maxText,"Starting up...");
-    text_layer_set_text(minText,"Starting up...");
     text_layer_set_text(healthText,"Starting up...");
+    text_layer_set_text(healthTextBottom,"Starting up...");
+    layer_add_child(window_get_root_layer(window), (Layer*)healthBar);
     layer_add_child(window_get_root_layer(window), (Layer*)accelText);
-    layer_add_child(window_get_root_layer(window), (Layer*)maxText);
-    layer_add_child(window_get_root_layer(window), (Layer*)minText);
     layer_add_child(window_get_root_layer(window), (Layer*)healthText);
+    layer_add_child(window_get_root_layer(window), (Layer*)healthTextBottom); 
 }
 
 static void window_unload(Window *window) {
     text_layer_destroy(accelText);
-    text_layer_destroy(maxText);
-    text_layer_destroy(minText);
     text_layer_destroy(healthText);
+    text_layer_destroy(healthTextBottom);
 }
 
 static void init(void) {
@@ -195,6 +229,7 @@ static void init(void) {
     });
     app_message_register_inbox_received(in_received_handler);
     app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());  
+    accel_service_set_sampling_rate(ACCEL_SAMPLING_50HZ);
     accel_raw_data_service_subscribe(HISTORY, accelSubscriber);
     window_stack_push(window, true);
 }
